41a5ec127dbabe11a9f12f5eb27517717a5f017a
Scott Olcott <solcott@mediciventures.com>
2019-05-10 15:57:22 -0600

    Fix issue with Android Q not writing maps to Parcels

--- geopackage-sdk/src/main/java/mil/nga/geopackage/db/SQLUtils.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/db/SQLUtils.java
@@ -1,9 +1,7 @@
 package mil.nga.geopackage.db;
 
 import android.content.ContentValues;
-import android.os.Parcel;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -23,20 +21,77 @@ public class SQLUtils {
     public static ContentValues quoteWrap(ContentValues values) {
         ContentValues quoteValues = null;
         if (values != null) {
-
-            Map<String, Object> quoteMap = new HashMap<>();
-            for (Map.Entry<String, Object> value : values.valueSet()) {
-                quoteMap.put(CoreSQLUtils.quoteWrap(value.getKey()), value.getValue());
+            quoteValues = new ContentValues();
+            for (Map.Entry<String, Object> valueSet : values.valueSet()) {
+                String key = CoreSQLUtils.quoteWrap(valueSet.getKey());
+                Object value = valueSet.getValue();
+                if (value instanceof String) {
+                    quoteValues.put(key, (String) value);
+                } else if (value == null) {
+                    quoteValues.putNull(key);
+                } else {
+                    Class<?> primitiveType = primitiveType(value.getClass());
+                    if (primitiveType == Integer.TYPE) {
+                        quoteValues.put(key, (Integer) value);
+                    } else if (primitiveType == Byte.TYPE) {
+                        quoteValues.put(key, (Byte) value);
+                    } else if (primitiveType == Long.TYPE) {
+                        quoteValues.put(key, (Long) value);
+                    } else if (primitiveType == Float.TYPE) {
+                        quoteValues.put(key, (Float) value);
+                    } else if (primitiveType == Double.TYPE) {
+                        quoteValues.put(key, (Double) value);
+                    } else if (primitiveType == Short.TYPE) {
+                        quoteValues.put(key, (Short) value);
+                    } else if (primitiveType == Boolean.TYPE) {
+                        quoteValues.put(key, (Boolean) value);
+                    } else {
+                        quoteValues.put(key, (byte[])value);
+                    }
+                }
             }
 
-            Parcel parcel = Parcel.obtain();
-            parcel.writeMap(quoteMap);
-            parcel.setDataPosition(0);
-            quoteValues = ContentValues.CREATOR.createFromParcel(parcel);
-            parcel.recycle();
         }
 
         return quoteValues;
+    }
+
+    /**
+     * Method that can be used to find primitive type for given class if (but only if)
+     * it is either wrapper type or primitive type; returns `null` if type is neither.
+     *
+     * @since 2.7
+     */
+    private static Class<?> primitiveType(Class<?> type) {
+        if (type.isPrimitive()) {
+            return type;
+        }
+
+        if (type == Integer.class) {
+            return Integer.TYPE;
+        }
+        if (type == Long.class) {
+            return Long.TYPE;
+        }
+        if (type == Boolean.class) {
+            return Boolean.TYPE;
+        }
+        if (type == Double.class) {
+            return Double.TYPE;
+        }
+        if (type == Float.class) {
+            return Float.TYPE;
+        }
+        if (type == Byte.class) {
+            return Byte.TYPE;
+        }
+        if (type == Short.class) {
+            return Short.TYPE;
+        }
+        if (type == Character.class) {
+            return Character.TYPE;
+        }
+        return null;
     }
 
 }
